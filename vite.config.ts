5c2bfa3f8c74186b37339ddd82ded58b3fcd12e9
Dennis Too (Techno Brain Global FZ LLC) <v-dennistoo@microsoft.com>
2026-04-30 12:30:00 +0300

    Added github actions workflow to allow viewing of web app via github pages

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,7 @@ import vue from '@vitejs/plugin-vue'
 import path from 'path'
 
 export default defineConfig({
+  base: '/astra-enterprises-frontend/',
   plugins: [vue()],
   resolve: {
     alias: {
